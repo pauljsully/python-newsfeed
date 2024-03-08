898e2dd099d160e584a62de31747bd0d760d1acf
Paul Sullivan <paul8sullivan@gmail.com>
2024-03-08 05:30:20 -0500

    api routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,34 @@
 from flask import Flask
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
 
+
 def create_app(test_config=None):
-  # set up app config
-  app = Flask(__name__, static_url_path='/')
-  app.url_map.strict_slashes = False
-  app.config.from_mapping(
-    SECRET_KEY='super_secret_key'
-  )
+    # set up app config
+    app = Flask(__name__, static_url_path='/')
+    app.url_map.strict_slashes = False
+    app.config.from_mapping(
+        SECRET_KEY='super_secret_key'
+    )
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
 
-  app.jinja_env.filters['format_url'] = filters.format_url
-  app.jinja_env.filters['format_date'] = filters.format_date
-  app.jinja_env.filters['format_plural'] = filters.format_plural
+    app.config.update(
+        DEBUG=True,
+        PROPAGATE_EXCEPTIONS=True
+    )
 
-  @app.route('/hello')
-  def hello():
-    return 'hello world'
-  app.register_blueprint(home)
-  app.register_blueprint(dashboard)
+    #app.run(debug=True)
+    if __name__ == "__main__":
+        app.run(debug=True)
 
-  
-  init_db(app)
+    # register routes
+    app.register_blueprint(home)
+    app.register_blueprint(dashboard)
+    app.register_blueprint(api)
 
-  
-  return app
-  
+    init_db(app)
+
+    return app
